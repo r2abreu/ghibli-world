1a525d42e85c377d291c65f324aa0133555f898b
r2abreu <r2abreu@pm.me>
2020-12-18 22:49:55 +0100

    Connect Ghibli API

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ghibli from '../api/ghibli';
 
 const App = () => {
+	const [ movies, setMovies ] = useState([]);
+
+	useEffect(() => {
+		search();
+	}, []);
+
+	const search = async () => {
+		const response = await ghibli('films');
+
+		console.log(response);
+	};
 	return <div>Hola</div>;
 };
 
